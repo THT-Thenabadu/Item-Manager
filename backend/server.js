4740cf821478ee1109f0947f87a62a32e62711c2
THT-Thenabadu <tathikathenabadu1@gmail.com>
2026-04-28 10:22:21 +0530

    commit1

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://item-manager-2ed2.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
